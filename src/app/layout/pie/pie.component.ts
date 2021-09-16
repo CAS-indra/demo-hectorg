4936990bb3c21e012ef4eeb85a946076271311e7
Hector Garcia Hernandez <hectorgarciahernandez.hgh@hotmail.com>
2021-09-16 17:59:26 +0200

    modulo layout con cabecera y pie y pipes

---
+++ src/app/layout/pie/pie.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pie',
+  templateUrl: './pie.component.html',
+  styleUrls: ['./pie.component.scss'],
+})
+export class PieComponent implements OnInit {
+  public desarrollador = 'Héctor García';
+  public fecha = new Date();
+  constructor() {}
+
+  ngOnInit(): void {}
+}
