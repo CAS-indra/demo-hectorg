1c848bcd4a5dbbb5afe9983b1991e1aa51ff03fa
Hector Garcia Hernandez <hectorgarciahernandez.hgh@hotmail.com>
2021-09-17 11:01:25 +0200

    plantillas y componentes angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  public proyectos = [
+    {
+      name: 'Aprender Angular',
+      status: 1,
+    },
+    {
+      name: 'Tener un buen trabajo',
+      status: 0,
+    },
+  ];
+  public color = 'blueviolet';
+  public value = 50;
+
+  public incrementar(): void {
+    this.value++;
+    console.log('value --> ', this.value);
+  }
+
+  public reducir(): void {
+    this.value--;
+    console.log('value --> ', this.value);
+  }
+
+  public multiplo(cantidad: number): number {
+    return cantidad * this.value;
+  }
+
   title = 'App demo de Hector';
 }
