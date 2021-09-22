db1719ba72fd44654e39a0ae659300a46efd2388
Hector Garcia Hernandez <hectorgarciahernandez.hgh@hotmail.com>
2021-09-22 12:02:45 +0200

    ruta environment y metodos CRUD

--- src/app/data/proyectos.service.ts
+++ src/app/data/proyectos.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root',
@@ -21,6 +22,8 @@ export class ProyectosService {
   //     status: 0,
   //   },
   // ];
+
+  private readonly url = environment.apiUrl + 'projects/';
   constructor(private http: HttpClient) {}
 
   public getProyectos(): any[] {
@@ -28,14 +31,24 @@ export class ProyectosService {
   }
 
   public getProyectos$(): Observable<{ data: any[] }> {
-    return this.http.get<{ data: any[] }>(
-      'https://proton-angular-builders.herokuapp.com/v1/projects',
-    );
+    return this.http.get<{ data: any[] }>(this.url);
   }
 
   public getProjectById(id: string): Observable<any> {
+    return this.http.get<any>(this.url + id).pipe(map(res => res.data));
+  }
+
+  public postProyecto$(proyecto: any): Observable<any> {
+    return this.http.post<{ data: any }>(this.url, proyecto).pipe(map(res => res.data));
+  }
+
+  public putProyecto$(proyecto: any): Observable<any> {
     return this.http
-      .get<any>('https://proton-angular-builders.herokuapp.com/v1/projects/' + id)
+      .put<{ data: any }>(this.url + proyecto.id, proyecto)
       .pipe(map(res => res.data));
+  }
+
+  public deleteProjectById(id: string): Observable<void> {
+    return this.http.delete<void>(this.url + id);
   }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,6 +4,7 @@
 
 export const environment = {
   production: false,
+  apiUrl: 'https://proton-angular-builders.herokuapp.com/v1/projects',
 };
 
 /*
